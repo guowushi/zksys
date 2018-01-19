a757dd5b1be5c85aaf22069ea00fa3755d7b3107
guowushi <guowushi@qq.com>
2018-01-19 14:15:00 +0800

    delete all

--- fastadmin/addons/upload/bootstrap.js
+++
@@ -1,8 +0,0 @@
-//修改上传的接口调用
-require(['upload'], function (Upload) {
-    var _FileUploaded = Upload.events.FileUploaded;
-    Upload.events.FileUploaded = function (up, file, info) {
-        //因为不同上传所返回的数据格式不同，你可以在这里进行处理
-        _FileUploaded.apply(this, [up, file, info]);
-    };
-});
--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: root
- * Date: 2018/1/18 0018
- * Time: 09:02
- */
